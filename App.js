b61b92a68d01c2c7fb72901d6fe2bbb6ca30f018
BlinkZoiLiu <Huypx.dev@gmail.com>
2023-05-13 09:39:17 +0700

    Add login logout with firebase

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import { initializeApp } from "firebase/app";
 
 import {
   useFonts as useOswald,
@@ -11,9 +12,18 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
 import { Navigation } from "./src/infrastructure/navigation";
 
-import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
-import { LocationContextProvider } from "./src/services/location/location.context";
-import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBH6EEFKfHHJ_yN7Zbza71Oz7DcZTLopVY",
+  authDomain: "mealstogo-86ff4.firebaseapp.com",
+  projectId: "mealstogo-86ff4",
+  storageBucket: "mealstogo-86ff4.appspot.com",
+  messagingSenderId: "437607300223",
+  appId: "1:437607300223:web:49acf013b1750c520a534a",
+};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -31,13 +41,9 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
